bcf40d54eec412c02b5975ae7679c2d95f030016
codeisneverodd <codeisneverodd@gmail.com>
2023-03-23 22:57:24 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "@/lib/@components/MainLayout";
+import { Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <MainLayout title="Home">
+      <Text>New Template</Text>
+    </MainLayout>
+  );
+}
